e7e98f032ea7001dd0e67fb6a87347dde31b23a0
Glitch (userminder) <none>
2018-03-22 20:54:41 +0000

    🐑👊 Checkpoint ./server.js:5883/100 ./package.json:5883/294

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
-var express = require('express');
-var app = express();
-//var https = require('https');
-var http = require('http');
-var bodyParser = require('body-parser');
-//var request = require('request');
+var express = require('express')
+var app = express()
+var http = require('http')
+var bodyParser = require('body-parser')
+//var request = require('request')
+
+app.use(express.static('public'))
+//app.set('trust proxy', 1)
+app.use(bodyParser.json())
 
 
-app.use(express.static('public'));
-//app.set('trust proxy', 1);
-app.use(bodyParser.json());
+//app.get("/", function (request, response) {
+//  response.sendFile(__dirname + '/views/index.html')
+//})
 
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-
-app.get("/dossier", function (req, resp) {
+app.get("/dossier", function(req, resp) {
   console.log("GET /dossier")
   console.log(req.params)
   getDossier(req.params.email, req.params.token,
     (userp) => {
-      resp.send(JSON.stringify(userp));  
+      resp.send(JSON.stringify(userp))
     }, 
     (error) => {
       
     }
-  );
-});
+  )
+})
 
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Listening on port ' + listener.address().port);
-});
+var listener = app.listen(process.env.PORT, function() {
+  console.log('The Userminder app is running on port ' + listener.address().port)
+})
 
 function getDossier(email, token, success, error) {
    var options = {
