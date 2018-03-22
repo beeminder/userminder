69ef4759bd373b856267577ed3671407ca45711c
Glitch (userminder) <none>
2018-03-22 22:43:09 +0000

    💦🍴 Checkpoint ./public/client.js:5883/21:7333/13 ./public/index.html:7333/15

--- public/client.js
+++ public/client.js
@@ -10,26 +10,26 @@ $(function() {
     console.log("could not paste. boo!")
   }
   
-  $("form#email").submit(function(e){
-    e.preventDefault();
+  $("form.raplet").submit(function(e){
+    e.preventDefault()
     $.getJSON(
       "/dossier", {
         email: $("#email").val(),
-        token: $("#token").val()
+        token: $("#token").val(),
       },
       function(data) {
         console.log(data)
-        $("#userinfo").append(formatDossier(data[0]));
-      });
-  });
-});
+        $("#userinfo").append(formatDossier(data[0]))
+      })
+  })
+})
 
 function formatDossier(doss) {
-  var div = $("<div></div>");
-  var bkg = doss.subscription?"vip":doss.is_payer?"prio2":"prio3";
-  div.addClass(bkg);
-  div.append("<a><h2></h2></a>");
-  div.find("h2").text(doss.username);
-  div.find("a").attr("href", "https://www.beeminder.com/"+doss.username);
-  return div;
+  var div = $("<div></div>")
+  var bkg = doss.subscription ? "vip" : doss.is_payer ? "prio2" : "prio3"
+  div.addClass(bkg)
+  div.append("<a><h2></h2></a>")
+  div.find("h2").text(doss.username)
+  div.find("a").attr("href", "https://www.beeminder.com/"+doss.username)
+  return div
 }
