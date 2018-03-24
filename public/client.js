8d976392a9c7161a93901fb33170e8bfea4fbee2
Glitch (userminder) <none>
2018-03-24 06:25:55 +0000

    💏👩 Checkpoint ./public/client.js:5883/1320:7333/56 ./public/index.html:5883/384

--- public/client.js
+++ public/client.js
@@ -1,5 +1,23 @@
 // ---------------------------------- 80chars --------------------------------->
 
+// Put the contents of the clipboard in the "clipboard" span
+function cbmonitor() {
+  var clipboard = ''
+  var textarea = document.getElementById("magic_textarea")
+  textarea.value = ''
+  textarea.focus()
+  if (document.execCommand("paste")) {
+    clipboard = textarea.value
+  } else {
+    clipboard = 'ERROR23' // if we see this then probably the Chrome extension
+                          // that's supposed to let us monitor the clipboard
+                          // isn't working.
+  }
+  document.getElementById("clipboard").textContent = clipboard
+}
+
+setInterval(cbmonitor, 1000)
+
 $(function() {
   if (document.execCommand("paste")) {
     console.log("pasted! yay!")
@@ -22,22 +40,6 @@ $(function() {
         $("#userinfo").append(formatDossier(data[0]))
         console.log(users);
       })
-  
-    // ok, this works whenever the submit button is pressed.
-    // it lets us get the clipboard contents as a string.
-    var clipboard = ''
-    var textarea = document.getElementById("visible_textarea")
-    textarea.value = ''
-    textarea.focus()
-    if (document.execCommand("paste")) {
-      clipboard = textarea.value
-    } else {
-      clipboard = 'ERROR23' // if we see this then probably the Chrome extension
-                            // that's supposed to let us monitor the clipboard
-                            // isn't working.
-    }
-    document.getElementById("clipboard").textContent = clipboard
-
   })
 })
 
@@ -48,10 +50,10 @@ function formatDossier(doss) {
   } else {
     var bkg = doss.subscription ? "vip" : doss.is_payer ? "prio2" : "prio3"
     div.addClass(bkg)
-    div.append("<a><h2></h2></a><span class='subs'></span>")
+    div.append("<a><h2></h2></a>")
     div.find("h2").text(doss.username)
     div.find("a").attr("href", "https://www.beeminder.com/"+doss.username)
-    div.append("<span>"+find(".subs").textdoss.subscription)
+    div.append("<span>"+doss.subscription+"</span>")
   }
   return div
 }
