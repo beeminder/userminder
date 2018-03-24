a3777974a8a0f4fad30f6d0b798640cd00be0029
Glitch (userminder) <none>
2018-03-24 06:03:55 +0000

    ☠️⛱ Checkpoint ./public/client.js:5883/762:7333/190

--- public/client.js
+++ public/client.js
@@ -1,3 +1,5 @@
+// ---------------------------------- 80chars --------------------------------->
+
 $(function() {
   if (document.execCommand("paste")) {
     console.log("pasted! yay!")
@@ -21,22 +23,34 @@ $(function() {
         console.log(users);
       })
   
-      // TODO: can we put the cursor in the text area before doing the following?
-      if (document.execCommand("paste")) {
-        console.log("pasted! yay!")
-      } else {
-        console.log("could not paste. boo!")
-      }
+    // ok, this works whenever the submit button is pressed.
+    // it lets us get the clipboard contents as a string.
+    var clipboard = ''
+    var textarea = document.getElementById("vt")
+    textarea.value = ''
+    textarea.focus()
+    if (document.execCommand("paste")) {
+      clipboard = textarea.value
+      console.log("CLIPBOARD: " + clipboard)
+    } else {
+      clipboard = 'ERROR'
+      console.log("ERROR, probably with Chrome extension that allows "
+                  + "clipboard monitoring")
+    }
 
   })
 })
 
 function formatDossier(doss) {
   var div = $("<div></div>")
-  var bkg = doss.subscription ? "vip" : doss.is_payer ? "prio2" : "prio3"
-  div.addClass(bkg)
-  div.append("<a><h2></h2></a>")
-  div.find("h2").text(doss.username)
-  div.find("a").attr("href", "https://www.beeminder.com/"+doss.username)
+  if (doss.username === undefined) {
+    div.append("<h2>"+doss.email_given+"</h2>")
+  } else {
+    var bkg = doss.subscription ? "vip" : doss.is_payer ? "prio2" : "prio3"
+    div.addClass(bkg)
+    div.append("<a><h2></h2></a>")
+    div.find("h2").text(doss.username)
+    div.find("a").attr("href", "https://www.beeminder.com/"+doss.username)
+  }
   return div
 }
