e7c64c0ac44562afcf9fb7a08e200e6357003301
Glitch (userminder) <none>
2018-03-30 06:54:58 +0000

    🌉🍢 Checkpoint ./public/client.js:5883/140

--- public/client.js
+++ public/client.js
@@ -65,16 +65,16 @@ function formatDossier(doss) {
   if (doss.username === undefined) {
     div.append("<h2>"+doss.email_given+" &rarr; NOT A BEEMINDER USER</h2>")
   } else {
-    var hover = JSON.stringify(doss)
+    var hover = JSON.stringify(doss) // show the full JSON on hover
     hover = hover.replace(/\"/g, "'")
-    console.log(hover)
     var bkg = doss.subscription ? "vip" : doss.is_payer ? "prio2" : "prio3"
     div.addClass(bkg)
     div.append(
       `<h2>${seemail} &rarr; ` + 
       `<a href="https://www.beeminder.com/${doss.username}"` + 
       `   target="_blank"` +
-      `   title="${hover}">${doss.username}</a></h2>` +
+      `   title="${hover}">${doss.username}</a>` + 
+      (seemail === doss.email ? '' : ` &rarr; ${doss.email}`) + '</h2>' +
       `<span>${doss.subscription}</span> ` +
       `<span>$${doss.pledged}</span> ` +
       `<span>since ${doss.since}</span>`
