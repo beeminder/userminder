8d5f379a44acd87057c0e2d023168549643e6c59
Glitch (userminder) <none>
2018-05-11 08:23:48 +0000

    💖⚽️ Checkpoint ./server.js:5883/325

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json())
 
 
 app.get("/dossier", function(req, resp) {
-  console.log("GET /dossier")
-  console.log(req.query)
+  //console.log("GET /dossier")
+  //console.log(req.query)
   getDossier(req.query.email, req.query.token,
     (userp) => { resp.send(JSON.stringify(userp)) }, 
-    (error) => { console.log("error in get dossier", error) }
+    (error) => { console.log("error in GET /dossier:", error) }
   )
 })
 
@@ -29,28 +29,23 @@ var listener = app.listen(process.env.PORT, function() {
 })
 
 function getDossier(email, token, success, error) {
-  console.log("DEBUGeml", email)
-   var options = {
+  var options = {
     host: 'www.beeminder.com',
     port: 443,
-    path: '/api/private/raplet.json?users[]='+encodeURI(email)
+    path: '/api/private/raplet.json?users[]='+encodeURIComponent(email)
                                   +"&auth_token="+token,
     method: 'GET',
   }
   var req = https.request(options, function(resp) {
     var data = ''
     resp.on('data', (chunk) => {
-      console.log("DEBUGraw", chunk)
-      console.log("DEBUGstr", JSON.stringify(chunk))
       data += chunk
     }).on('end', () => {
-      var userd = JSON.parse(data)
-      console.log("DEBUGdat", data)
-      success(userd)
+      success(JSON.parse(data))
     })
   })
   req.on('error', (e) => {
-    console.log('problem with request: ' + e.message)
+    console.log('problem with request:', e.message)
     error(e.message)
   })
   req.write('')
