770fb6be933f096c996975c6fb08d470655807e6
Glitch (userminder) <none>
2019-04-04 00:20:53 +0000

    Import of beeminder/userminder

---
+++ public/client.js
@@ -0,0 +1,127 @@
+// Determine mode
+const urlParams = new URLSearchParams(window.location.search);
+const mode = urlParams.get('mode');
+
+// Initialise
+let serverURL
+let clipboard
+
+
+if(mode==="desktop"){
+  const electron = require('electron')
+  const shell = electron.shell
+  clipboard = electron.clipboard
+  const port = electron.remote.getGlobal('port')
+  serverURL = `http://localhost:${port}`
+
+  $(() => {
+    console.log("Client page is ready")
+    let external = $(".external")
+    external.data("link", external.prop("href"))
+    external.prop("href", "#");
+    external.click(function(event){
+      let link = $(event.target).data("link")
+      console.log(`Open external: ${link}`)
+      shell.openExternal(link)
+    })
+    
+  })
+} else{
+  serverURL = ''  
+}
+// ---------------------------------- 80chars --------------------------------->
+// Global variable w/ currently extracted email address (email address we see)
+var seemail = '' 
+
+// Simple in-memory store: use this to keep track of what email addresses we've
+// already looked up and don't re-add them to the page.
+var seen = {}
+
+function processInput(input){
+  seemail = extract_email(input)
+  if (seemail) {
+    console.log(`Found email ${seemail}`)
+    //document.getElementById("email_box").value = email
+    document.getElementById("email_span").textContent = 
+      seemail // + (seen[seemail] ? ' (already fetched!)' : '')
+  } 
+  
+  if (seemail !== '' && !seen[seemail]) {
+    console.log("Email is new")
+    seen[seemail] = true
+    $.getJSON(
+      `${serverURL}/dossier`, {
+        email: seemail,
+        token: document.getElementById('token').value
+      },
+      function(data) {
+        console.log(`Received data ${data}`)
+        $("#userinfo").append(formatDossier(data[0]))
+      })
+  }
+}
+
+// Put the contents of the clipboard in the magic "clipboard" span
+function cbmonitor() {
+  var magic_textarea = document.getElementById("clipboard")
+  const clipboardContents = clipboard.readText()
+  magic_textarea.value = clipboardContents
+  processInput(clipboardContents)
+}
+
+// Take a string and return the first email address you find in it
+function extract_email(s) {
+  if (typeof(s) !== "string") { // pretty sure this check is superfluous now
+    console.log(`ERROR3: extract_email("${s}")`)
+    return ''
+  }
+  var erx = // eg a@b.c or a+b@c-d.e or even +-@x.co or _@.a or +@-b
+    /[\w\-\.\_\+]+\@[\w\-\.\_]+[a-zA-Z]/
+  var matches = s.match(erx)
+  return matches === null ? '' : matches[0]
+}
+
+
+let startedMonitoring = false
+// This is what runs when the page loads
+$(function() {  
+  $("form.raplet").submit(function(e) { // start monitoring when got auth token
+    e.preventDefault() // do we need this?
+    if(!startedMonitoring){
+      if(mode === "desktop"){
+        setInterval(cbmonitor, 1000)
+      }else{
+        $("#clipboard").on('input', function(event) {
+          console.log("Input changed")
+          processInput($(event.target).val())
+        })
+      }
+      startedMonitoring = True
+    }
+  })
+}) 
+
+function formatDossier(doss) {
+  var div = $("<div></div>") // would it be safer/clearer to find this by id?
+  if (doss.username === undefined) {
+    div.append("<h2>"+doss.email_given+" &rarr; NOT A BEEMINDER USER</h2>")
+  } else {
+    var hover = JSON.stringify(doss) // show the full JSON on hover
+    hover = hover.replace(/\"/g, "'")
+    var bkg = doss.subscription ? "vip" : doss.is_payer ? "prio2" : "prio3"
+    div.addClass(bkg)
+    div.append(
+      `<h2>${seemail} &rarr; ` + 
+      `<a href="https://www.beeminder.com/${doss.username}"` + 
+      `   target="_blank"` +
+      `   title="${hover}">${doss.username}</a>` + 
+      (seemail === doss.email ? '' : ` &rarr; ${doss.email}`) + '</h2>' +
+      `<span>${doss.subscription}</span> ` +
+      `<span>$${doss.pledged}</span> ` +
+      `<span>since ${doss.since}</span>`
+    )
+  }
+  return div
+}
+
+// ---------------------------------- 80chars --------------------------------->
