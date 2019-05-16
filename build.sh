3ad3d6bc1cdf08f5b89cbc48ee2ad4f9d34e48b2
Chris Leong <walkraft@gmail.com>
2019-05-17 03:12:44 +1000

    Now only runs on folders

--- build.sh
+++ build.sh
@@ -3,9 +3,14 @@ echo Starting build
 electron-packager --out releases --platform=darwin,linux,win32 --overwrite . UserMinder
 
 #Zip files
+echo "Now completing releases"
 cd releases
-for d in *; do
-    echo $d
-    zip -r $d.zip $d
+for dir in ./*/; do
+    dir2=${dir:2}
+    dir3=${dir2%?}
+    echo $dir3
+    zip -q -r $dir3.zip $dir3
 done;
 cd ..
+
+    #
