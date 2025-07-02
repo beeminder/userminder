a4e6ab77144311888f1b60ca2b327ccf7f09ed7a
Glitch (userminder) <none>
2025-07-02 01:29:01 +0000

    ⚙️🕞 Updated with Glitch

---
+++ public/preload.js
@@ -0,0 +1,29 @@
+electron = require('electron')
+
+// Determine mode
+const urlParams = new URLSearchParams(window.location.search);
+let port = electron.remote.getGlobal('port')
+
+let clipboard = electron.clipboard
+let shell = electron.shell
+
+function readClipboard(){
+    return clipboard.readText()
+}
+
+window.addEventListener("message", (event) => {
+    if (event.source != window) return
+    if (event.data.type && (event.data.type == "READCLIP_REQ")) {
+      window.postMessage({type: "READCLIP_ANS", text: readClipboard()}, "*")
+    }
+    if (event.data.type && (event.data.type == "GETPORT_REQ")) {
+        window.postMessage({type: "GETPORT_ANS", text: port}, "*")
+    }
+    if (event.data.type && (event.data.type == "OPENLINK_REQ")) {
+        shell.openExternal(event.data.link)
+    }
+  },
+  false
+)
+
+console.log("Finished preloading")
